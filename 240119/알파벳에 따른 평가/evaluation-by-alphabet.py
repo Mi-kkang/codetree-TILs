65b8bcb7f5ab02fe8449476143168f66ab731696
Mi-kkang <135505904+Mi-kkang@users.noreply.github.com>
2024-01-19 08:54:08 +0900

    Title: 알파벳에 따른 평가, Time: 111ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240119/알파벳에 따른 평가/evaluation-by-alphabet.py
@@ -0,0 +1,19 @@
+alph = input()
+
+if alph == 'S' :
+    print('Superior')
+
+elif alph == 'A' :
+    print('Excellent')
+
+elif alph == 'B' :
+    print('Good')
+
+elif alph == 'C' :
+    print('Usually')
+
+elif alph == 'D' :
+    print('Effort')
+
+else :
+    print('Failure')
