6cd3905372b8175a0c0dd0f40fac28b24ffdf40c
Mi-kkang <135505904+Mi-kkang@users.noreply.github.com>
2024-01-24 14:58:39 +0900

    Title: 비교 연산, Time: 121ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240124/비교 연산/comparison-operator.py
@@ -0,0 +1,31 @@
+a, b = map(int,input().split())
+
+if (a >= b) == True :
+    print(1)
+else :
+    print(0)
+
+if (a > b) == True :
+    print(1)
+else :
+    print(0)
+
+if (b >= a) == True :
+    print(1)
+else :
+    print(0)
+
+if (b > a) == True :
+    print(1)
+else :
+    print(0)
+
+if (a == b) == True :
+    print(1)
+else :
+    print(0)
+
+if (a != b) == True :
+    print(1)
+else :
+    print(0)
