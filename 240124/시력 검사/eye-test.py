cca78850ec43d7b65c05e49b4e940f39ce7eafc0
Mi-kkang <135505904+Mi-kkang@users.noreply.github.com>
2024-01-24 15:18:14 +0900

    Title: 시력 검사, Time: 117ms, Memory: 25MB, Status: Passed - CodeTree

---
+++ 240124/시력 검사/eye-test.py
@@ -0,0 +1,11 @@
+one = float(input())
+other = float(input())
+
+if one >= 1.0 and other >= 1.0 :
+    print('High')
+
+elif one >= 0.5 and other >= 0.5 :
+    print('Middle')
+
+else :
+    print('Low')
