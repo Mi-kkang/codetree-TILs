acb1664dbb4e78fb03123aa1f73c297a751b02e3
Mi-kkang <135505904+Mi-kkang@users.noreply.github.com>
2024-01-24 15:06:52 +0900

    Title: 4번의 크기 비교, Time: 108ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240124/4번의 크기 비교/4-time-comparison.py
@@ -0,0 +1,30 @@
+a = int(input())
+num_list = list(map(int,input().split()))
+b = num_list[0]
+c = num_list[1]
+d = num_list[2]
+e = num_list[3]
+
+if (a > b) == True :
+    print(1)
+
+else :
+    print(0)
+
+if (a > c) == True :
+    print(1)
+
+else :
+    print(0)
+
+if (a > d) == True :
+    print(1)
+
+else :
+    print(0)
+
+if (a > e) == True :
+    print(1)
+
+else :
+    print(0)
